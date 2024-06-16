0a627bbf3dbca0cc611b04d090cfcb949da94530
Jason Reed <jcreed@gmail.com>
2024-06-16 14:56:09 -0400

    Include more stuff in storybits panel

--- src/storybits-panel.tsx
+++ src/storybits-panel.tsx
@@ -12,13 +12,16 @@ export function storybitsPanel(state: AppState, dispatch: Dispatch): JSX.Element
   const renderItems = items.flatMap((item, ix) => {
     const { tags, attrs, body, date, file, meta } = item;
     const t = typeof (meta);
-    if (!tags.includes('storybits')) return [];
+    if (!(tags.includes('storybits') || file == 'CONWORLD')) return [];
     const id = meta?.id;
     const idShower = (id == undefined) ? idButton(ix, dispatch) : id;
     // XXX This use of ix as key is bad --- I should be using id once I ensure every item has id
+    const maybeTitle = attrs.title != undefined ? <h5>{attrs.title}</h5> : undefined;
+    const { title: _, ...otherAttrs } = attrs;
+    const maybeAttrs = Object.keys(otherAttrs).length > 0 ? 'otherAttrs=' + JSON.stringify(otherAttrs) : undefined;
     return [<tr key={ix}><td>{date}<br />
       <span className="guid">{idShower}</span></td>
-      <td>{renderTags(tags, dispatch)}</td><td><pre>{body}</pre></td></tr>];
+      <td>{renderTags(tags, dispatch)}</td><td>{maybeTitle}{maybeAttrs}<pre>{body}</pre></td></tr>];
   });
   return <table className="zebra"><tbody>{renderItems}</tbody></table>;
 }
