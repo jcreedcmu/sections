32fd4200281c22e252bf43835b7eb6e8cba1a036
Jason Reed <jcreed@gmail.com>
2024-06-17 12:49:57 -0400

    Add add-titles.ts script

---
+++ migration/add-titles.ts
@@ -0,0 +1,38 @@
+import { get_all_items } from '../server/notes-lib'
+import { parsed_item_of_item } from '../server/notes-lib-pure';
+
+const items = get_all_items().map(parsed_item_of_item);
+
+type Renaming = {
+  guid: string,
+  title: string,
+};
+const renamings: Renaming[] = [];
+
+items.forEach(item => {
+  if (item.body.match(/Bah, all the links/)) {
+    return;
+  }
+  if (item.tags.includes('storybits-summary')) {
+    item.body.split(/\n/).filter(x => x.length).forEach(line => {
+      let m;
+      if (m = line.match(/- link:\[[A-Z]+\/([-a-f0-9]+)\]\[.*?\] (.*)/)) {
+        //        console.log(m[1], m[2]);
+        const guid = m[1];
+        let title = m[2];
+        const ratingRe = / \(..?\)/;
+        if (title.match(ratingRe)) {
+          title = title.replace(ratingRe, '');
+        }
+        const matchedItem = items.find(it => it.meta?.id == guid);
+        if (matchedItem == undefined) {
+          console.log(`### Couldn't find ${guid}`);
+          return;
+        }
+        renamings.push({ guid, title });
+      }
+    });
+  }
+});
+
+console.log(JSON.stringify(renamings, null, 2));
