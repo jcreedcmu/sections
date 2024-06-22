7ba6e165dc28f70f607e4397f8ba91d7de348b83
Jason Reed <jcreed@gmail.com>
2024-06-22 10:54:49 -0400

    Modify entry id parsing a little

--- src/render-item-body.tsx
+++ src/render-item-body.tsx
@@ -1,10 +1,26 @@
 import * as React from 'react';
 import { Dispatch } from './action';
 
-export function renderLink(target: string, text: string, dispatch: Dispatch): JSX.Element {
+function getEntryId(target: string): string | undefined {
+  if (0) {
+    // Here's some code whereby I could be more lenient and match raw
+    // guids, but I think I'd prefer not to and be more explicit in my
+    // data.
+    if (target.length == 36 && target.match(/^[-a-f0-9]+$/)) {
+      return target;
+    }
+  }
   const m = target.match(/^([A-Z]*?)\/(.*)$/);
   if (m) {
-    return <a href={`#entry=${m[2]}`}>{text}</a >;
+    return m[2];
+  }
+  return undefined;
+}
+
+export function renderLink(target: string, text: string, dispatch: Dispatch): JSX.Element {
+  const entryId = getEntryId(target);
+  if (entryId != undefined) {
+    return <a href={`#entry=${entryId}`}>{text}</a >;
   }
   else {
     return <a target="_blank" href={target}>{text}</a>;
